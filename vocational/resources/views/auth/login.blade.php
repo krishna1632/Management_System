22025074804e1c612bca2d8a16d69fc38d7fea29
pra8953 <prabhatkumar6920@gmail.com>
2024-11-01 13:46:31 +0530

    xcvbnm

--- vocational/resources/views/auth/login.blade.php
+++ vocational/resources/views/auth/login.blade.php
@@ -2,6 +2,8 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
+    
+
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
